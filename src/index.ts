e4d0a62748382c4d29e9e6f473f105aedd3f298a
Lucas Moraes <lucas.moraes88@yahoo.com>
2024-11-07 18:07:12 -0300

    implementing the error handling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { bookRoutes } from "./interface/routes/bookRoutes";
+import { errorHandler } from "./interface/middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", bookRoutes);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
