3124d1abc8d03fea73769bb4d7228817251ac4cd
Lucas Moraes <lucas.moraes88@yahoo.com>
2024-11-12 23:35:06 -0300

    Implementing the documentation

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import { bookRoutes } from "./interface/routes/bookRoutes";
-import { errorHandler } from "./interface/middleware/errorHandler";
-import { logger } from "./infrastructure/logger";
+import { bookRoutes } from './interface/routes/bookRoutes';
+import { errorHandler } from './interface/middleware/errorHandler';
+import { logger } from './infrastructure/logger';
+import { setupSwagger } from './interface/swagger';
 
 const app = express();
 
 app.use(express.json());
-app.use("/api", bookRoutes);
+app.use('/api', bookRoutes);
 app.use(errorHandler);
+setupSwagger(app);
 
 app.listen(3000, () => {
-  logger.info("Server is running on port 3000");
+  logger.info('Server is running on port 3000');
 });
